825deed39cfda712ff15729b67ee33b0f81c9401
xingjin <xingjin@fayfans.com>
2019-07-10 19:44:43 +0800

    test

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"></HelloWorld>
-    <div>111111222222</div>
+    <div><<<<<<>>>>>>>>>>>></div>
   </div>
 </template>
 
